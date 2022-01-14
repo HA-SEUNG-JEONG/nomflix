373b3c8b285084c3cb3f5753bec32d7f1dd5136e
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-01-14 13:07:18 +0900

    Home Screen Part One,Installing ReactQuery

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,4 +1,11 @@
+import { useQuery } from "react-query";
+import { getMovies } from "./../api";
+
 function Home() {
+  const { data, isLoading, error } = useQuery(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
   return <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}></div>;
 }
 

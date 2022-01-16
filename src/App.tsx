e3df32a8e104afd5e69397887fae577645c88b17
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-01-16 14:03:25 +0900

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./Routes/Home";
 import Tv from "./Routes/Tv";
 import Search from "./Routes/Search";
@@ -6,11 +6,11 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <Switch>
         {/* react-router가 두 개의 path에서 같은 component를 rendering 하도록 할 수 있음 */}
-        <Route path={["/","/movies/:movieId"]}> 
+        <Route exact path={["/", "/movies/:movieId"]}>
           <Home />
         </Route>
         <Route path="/tv">
@@ -20,7 +20,7 @@ function App() {
           <Search />
         </Route>
       </Switch>
-    </Router>
+    </BrowserRouter>
   );
 }
 

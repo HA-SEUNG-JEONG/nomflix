a7ebbb815b44ea1a4b31a2640002de3bafd62c25
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-01-22 14:40:32 +0900

    react-helmet issue 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Home from "./Routes/Home";
-import Tv from "./Routes/Tv";
-import Search from "./Routes/Search";
+import Movie from "./Routes/Movie";
+import { Tv } from "./Routes/Tv";
+import { Search } from "./Routes/Search";
 import Header from "./components/Header";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Header />
-      <Switch>
-        {/* react-router가 두 개의 path에서 같은 component를 rendering 하도록 할 수 있음 */}
-        <Route exact path={["/", "/movies/:movieId"]}>
-          <Home />
-        </Route>
-        <Route path="/tv">
-          <Tv />
-        </Route>
-        <Route path="/search">
-          <Search />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HelmetProvider>
+          <Helmet>
+            <title>Nomflix</title>
+          </Helmet>
+        </HelmetProvider>
+        <Header />
+        <Switch>
+          {/* react-router가 두 개의 path에서 같은 component를 rendering 하도록 할 수 있음 */}
+          <Route exact path={["/", "/movies/:movieId"]}>
+            <Movie />
+          </Route>
+          <Route path="/tv">
+            <Tv />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 

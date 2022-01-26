3922f599b6d88e74b4bb6b16307da9c607cf754a
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-01-27 00:05:43 +0900

    react-helmet 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,11 @@ import Movie from "./Routes/Movie";
 import { Tv } from "./Routes/Tv";
 import { Search } from "./Routes/Search";
 import Header from "./components/Header";
-import { Helmet, HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
     <>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <HelmetProvider>
-          <Helmet>
-            <title>Nomflix</title>
-          </Helmet>
-        </HelmetProvider>
         <Header />
         <Switch>
           {/* react-router가 두 개의 path에서 같은 component를 rendering 하도록 할 수 있음 */}
